94577acbfcdfcd49c94ac4289010856374ff482e
Mohammed Abdulrahman <82520219+v-abdrahman@users.noreply.github.com>
2021-05-25 15:43:06 +0530

    Add files via upload

--- PotentiometerSensor/Properties/AssemblyInfo.cs
+++ PotentiometerSensor/Properties/AssemblyInfo.cs
@@ -1,15 +1,18 @@
-﻿using System.Reflection;
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RGBLED")]
+[assembly: AssemblyTitle("PotentiometerSensor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RGBLED")]
+[assembly: AssemblyProduct("PotentiometerSensor")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
