f7d4fb21d86ca7f02f75ea52dd33fc4b35218350
Mohammed Abdulrahman <82520219+v-abdrahman@users.noreply.github.com>
2021-06-08 14:53:43 +0530

    Add files via upload

---
+++ SpeechTranslator/CS/ConstantParam.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace SpeechTranslator
+{
+    class ConstantParam
+    {           
+        //Token Property
+        public static string subscriptionKey = "47fe38401a6e45699b1ff76c8d3681d1";
+    }
+}
