710e32a818bd69dfdbd572f12011b24b0eb263e5
Mohammed Abdulrahman <82520219+v-abdrahman@users.noreply.github.com>
2021-06-15 12:01:26 +0530

    Update ConstantParam.cs

--- SpeechTranslator/CS/ConstantParam.cs
+++ SpeechTranslator/CS/ConstantParam.cs
@@ -5,6 +5,6 @@ namespace SpeechTranslator
     class ConstantParam
     {           
         //Token Property
-        public static string subscriptionKey = "47fe38401a6e45699b1ff76c8d3681d1";
+        public static string subscriptionKey = "REPLACE THIS WITH YOUR AZURE COGNITIVE SERVICES SUBSCRIPTION KEY";
     }
 }
