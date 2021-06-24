2406dd45175b481f4598984ed664941c2c1de71d
rsameser <68604965+rsameser@users.noreply.github.com>
2021-06-24 04:51:08 -0700

    Delete IoTEnterpriseDefault directory

--- IoTEnterpriseDefault/CS/SmartDisplay.Contracts/Interfaces/ICustomContentService.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-
-namespace SmartDisplay.Contracts
-{
-    public interface ICustomContentService : ISmartDisplayService
-    {
-        T GetContent<T>(string key);
-    }
-}
